b13b68db665c2a95600461d20068fd8882ddc1aa
Chen Peleg <chenpeleg@gmail.com>
2025-01-05 17:56:07 +0200

    partially working example

--- src/services/hook/useService.ts
+++ src/services/hook/useService.ts
@@ -1,8 +1,7 @@
 
 import { useServicesContext } from '../context/GlobalServicesContext.tsx';
 import { ResourceProviderConstructor } from '../resolvers/ServiceResolverClass.ts';
-
-export const useService = (serviceClass : ResourceProviderConstructor) => {
+export const useService =<T extends ResourceProviderConstructor> (serviceClass : T) :InstanceType<T>=> {
     const services = useServicesContext();
-    return  services.getService(serviceClass);
+    return  services.getService(serviceClass)  as InstanceType<T> ;
 };
