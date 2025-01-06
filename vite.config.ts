f04fa5d43a3c63e0ad5e14b0fa5561ab12ac0c24
Chen Peleg <chenpeleg@gmail.com>
2025-01-06 15:09:45 +0200

    ready for publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: 'examples',
+  build: {
+    lib: {
+      entry:  'src/index.ts' ,
+      name: 'react-services-locator',
+      fileName: 'index',
+    },
+  },
+
   plugins: [react()],
 })
