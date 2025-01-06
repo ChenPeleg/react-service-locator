e7bcba7bf886c0834f3777633740589e66692832
Chen Peleg <chenpeleg@gmail.com>
2025-01-06 14:37:43 +0200

    added licence

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {AbstractBaseService} from './AbstractBaseService.ts';
+export {ServicesResolver} from './ServiceResolverClass.ts';
+export {ServicesProvider, ServicesProviderContext} from './ServicesProvider.tsx';
+export {useService} from './useService.ts';
