82fdb9058e84740ade8f3547d047222703ab7e3a
Chen Peleg <chenpeleg@gmail.com>
2025-01-07 09:50:10 +0200

    only esm publishing

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,26 @@ import dts from 'vite-plugin-dts';
 import { peerDependencies } from './package.json';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'react-services-locator',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['cjs', 'es'],
+    build: {
+        lib: {
+            entry: './src/index.ts',
+            name: 'react-services-locator',
+            fileName: () => `index.js`,
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: [...Object.keys(peerDependencies)],
+        },
+        sourcemap: true,
+        emptyOutDir: true,
     },
-    rollupOptions: {
-      external: [...Object.keys(peerDependencies)],
-    },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
-  plugins: [
-    dts({
-      outDir: 'dist',
-      insertTypesEntry: true,
-      cleanVueFileName: true,
-      rollupTypes: true,
-      tsconfigPath: "./tsconfig.app.json",
-    }),
-  ],
+    plugins: [
+        dts({
+            outDir: 'dist',
+            insertTypesEntry: true,
+            cleanVueFileName: true,
+            rollupTypes: true,
+            tsconfigPath: './tsconfig.app.json',
+        }),
+    ],
 });
