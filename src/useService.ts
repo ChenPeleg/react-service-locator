2910c7c8d68fd39eed630d244745a1ddc2698646
Chen Peleg <chenpeleg@gmail.com>
2025-01-07 11:56:46 +0200

    added examples

--- src/useService.ts
+++ src/useService.ts
@@ -2,8 +2,28 @@ import {  ServicesProviderContext } from './ServicesProvider.tsx';
 import { ServiceConstructorClass } from './ServiceResolverClass.ts';
 import { useContext } from 'react';
 
-// Overload signatures for useService hook
+/**
+ * Hook to retrieve a service instance by its class.
+ * @template T
+ * @param {T} serviceClass - The service class to retrieve.
+ * @returns {InstanceType<T>} The instance of the requested service.
+ * @example
+ * // Assuming DataService is a service class
+ * const dataService = useService(DataService);
+ * dataService.getData('https://example.com');
+ */
 export function useService<T extends ServiceConstructorClass>(serviceClass: T): InstanceType<T>;
+/**
+ * Hook to retrieve multiple service instances by their classes.
+ * @template T
+ * @param {T[]} serviceClass - An array of service classes to retrieve.
+ * @returns {{ [K in keyof T]: InstanceType<T[K]> }} An array of instances of the requested services.
+ * @example
+ * // Assuming DataService and ProfileDataService are service classes
+ * const [dataService, profileDataService] = useService([DataService, ProfileDataService]);
+ * dataService.getData('https://example.com');
+ * profileDataService.getProfileData();
+ */
 export function useService<T extends ServiceConstructorClass[]>(serviceClass: [...T]): { [K in keyof T]: InstanceType<T[K]> };
 
 
